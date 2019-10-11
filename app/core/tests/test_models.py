cf4a207029bc6bf157ecdc9ab008a2e3142dfe62
Igor Donin <igordonin@gmail.com>
2019-10-11 11:59:20 -0300

    Adicionando Receita

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -49,10 +49,20 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(tag), tag.name)
 
-    def test_model_str(self):
+    def test_ingredient_str(self):
         ingredient = models.Ingredient.objects.create(
             user=sample_user(),
             name='Cucumber'
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom',
+            time_in_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
